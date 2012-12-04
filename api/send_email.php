1b03e2f9794ca573dde97939a63c92786ad9ffed
Noah Hamann <njhamann@gmail.com>
2012-12-04 13:47:53 +0000

    email formatting

--- api/send_email.php
+++ api/send_email.php
@@ -1,15 +1,14 @@
 <?php
-header('Content-type: application/json');
-if (isset($_REQUEST['email'])){
-    //send email
-    $email = $_REQUEST['email'];
-    $subject = 'Message from noahhamann.com contact form';
-    $message = $_REQUEST['message'];
-    mail("njhamann@gmail.com", $subject, $message, "From:" . $email);
-    $resp = '{"success":1}';
-}else{
-    $resp = '{"success":0}';
-}
-
-echo $resp;
+    header('Content-type: application/json');
+    if (isset($_REQUEST['email'])){
+        //send email
+        $email = $_REQUEST['email'];
+        $subject = 'Message from noahhamann.com contact form';
+        $message = "from: ".$email."\n\n".$_REQUEST['message'];
+        mail("njhamann@gmail.com", $subject, $message, "From: robot@noahhamann.com");
+        $resp = '{"success":1}';
+    }else{
+        $resp = '{"success":0}';
+    }
+    echo $resp;
 ?>
