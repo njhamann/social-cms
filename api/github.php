0ccc780e6ef849993002cd2fd5235ac2dadb3fcd
Noah Hamann <njhamann@gmail.com>
2014-01-23 01:04:35 +0000

    github changes

--- api/github.php
+++ api/github.php
@@ -8,6 +8,27 @@ curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 
+function tokenTruncate($string, $your_desired_width) {
+    if(strlen($string) > $your_desired_width){
+        $parts = preg_split('/([\s\n\r]+)/', $string, null, PREG_SPLIT_DELIM_CAPTURE);
+        $parts_count = count($parts);
+
+        $length = 0;
+        $last_part = 0;
+        for (; $last_part < $parts_count; ++$last_part) {
+            $length += strlen($parts[$last_part]);
+            if ($length > $your_desired_width) { 
+        
+                break; 
+            }
+        }
+
+        return implode(array_slice($parts, 0, $last_part)).'...';
+    }else{
+        return $string;
+    }
+}
+
 $json = curl_exec($ch);
 curl_close($ch);
 //$json = file_get_contents($url);
@@ -44,6 +65,14 @@ for($i=0; $i<10; $i++){
         $copy = 'I '.$item->payload->action.' watching '.$repo; 
     }else if($type == 'FollowEvent'){
         $copy = 'I started following <a href="'.$item->payload->target->html_url.'">'.$item->payload->target->login.'</a>'; 
+    }else if($type == 'PullRequestReviewCommentEvent'){
+        $copy = 'I commented on a pull request on ' . $repo;
+        $commits = array(tokenTruncate($item->payload->comment->body, 200));
+    }else if($type == 'PullRequestEvent'){
+        $copy = 'Pull request event on ' . $repo;
+    }else if($type == 'IssueCommentEvent'){
+        $copy = 'I commented on an issue at ' . $repo;
+        $commits = array(tokenTruncate($item->payload->comment->body, 200));
     }
     $node = array(
         'id' => $item->id,
